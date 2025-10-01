b658ceb1438c797e393d69abf7c62ed1f0b79b4b
Ellie Wright <ellie952@revature.net>
2025-10-01 12:04:31 -0500

    Update logger.js
    Updated the transport to correctly generate loggerFile.log into src directory

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,19 +1,19 @@
-const {createLogger, transports, format} = require("winston");
+const { createLogger, transports, format } = require("winston");
 const { combine, timestamp, label, prettyPrint } = format;
 
 const logger = createLogger({
     format: combine(
-    label({ label: 'HeartSync Backend' }),
-    timestamp(),
-    prettyPrint()
+        label({ label: 'HeartSync Backend' }),
+        timestamp(),
+        prettyPrint()
     ),
     transports: [
         new (transports.Console)(),
-        new (transports.File)({filename: 'loggerFile.log'})
+        new (transports.File)({ filename: 'src/loggerFile.log' })
     ]
 });
 
-function loggerMiddleware(req, res, next){
+function loggerMiddleware(req, res, next) {
     logger.info(`Incoming ${req.method} : ${req.url}`);
     next();
 }
