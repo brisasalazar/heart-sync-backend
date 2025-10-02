865961f9e59b95f593a14406cf272224595ed148
Ellie Wright <ellie952@revature.net>
2025-10-02 14:02:50 -0500

    Add edge case unit tests for playlistBuilderService

--- src/__test__/playlistBuilderService.test.js
+++ src/__test__/playlistBuilderService.test.js
@@ -20,12 +20,15 @@ jest.mock("../service/lastFMService.js", () => ({
 jest.mock("../service/spotifyService.js", () => ({
     getTrackURI: jest.fn(() => "SpotifyURI"),
     addTracksToPlaylist: jest.fn(() => "playlistId")
-}))
+}));
 
 describe("Playlist Builder service layer", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     test("Successfully returns a list of 100 Spotify URIs", async () => {
         // Arrange
-
         // Act
         const spotifyURIs = await populatePlaylist("playlistId", "genre", "artistName");
 
@@ -39,6 +42,60 @@ describe("Playlist Builder service layer", () => {
         expect(addTracksToPlaylist).toHaveBeenCalledWith(
             "playlistId",
             expect.arrayContaining(Array(100).fill("SpotifyURI"))
+        );
+    });
+
+    test("Successfully returns a list of 50 Spotify URIs based on genre", async () => {
+        // Arrange
+        // Act
+        const spotifyURIs = await populatePlaylist("playlistId", "genre");
+
+        // Assert
+        expect(spotifyURIs).toHaveLength(50);
+
+        expect(getTracksByGenre).toHaveBeenCalledTimes(1);
+        expect(getTracksByArtist).toHaveBeenCalledTimes(0);
+        expect(getTrackURI).toHaveBeenCalledTimes(50);
+        expect(addTracksToPlaylist).toHaveBeenCalledTimes(1);
+        expect(addTracksToPlaylist).toHaveBeenCalledWith(
+            "playlistId",
+            expect.arrayContaining(Array(50).fill("SpotifyURI"))
+        );
+    });
+
+    test("Successfully returns a list of 50 Spotify URIs based on artist", async () => {
+        // Arrange
+        // Act
+        const spotifyURIs = await populatePlaylist("playlistId", null, "artist");
+
+        // Assert
+        expect(spotifyURIs).toHaveLength(50);
+
+        expect(getTracksByGenre).toHaveBeenCalledTimes(0);
+        expect(getTracksByArtist).toHaveBeenCalledTimes(1);
+        expect(getTrackURI).toHaveBeenCalledTimes(50);
+        expect(addTracksToPlaylist).toHaveBeenCalledTimes(1);
+        expect(addTracksToPlaylist).toHaveBeenCalledWith(
+            "playlistId",
+            expect.arrayContaining(Array(50).fill("SpotifyURI"))
+        );
+    });
+
+    test("Successfully returns an empty list of Spotify URIs when no genre/artist is given", async () => {
+        // Arrange
+        // Act
+        const spotifyURIs = await populatePlaylist("playlistId");
+
+        // Assert
+        expect(spotifyURIs).toHaveLength(0);
+
+        expect(getTracksByGenre).toHaveBeenCalledTimes(0);
+        expect(getTracksByArtist).toHaveBeenCalledTimes(0);
+        expect(getTrackURI).toHaveBeenCalledTimes(0);
+        expect(addTracksToPlaylist).toHaveBeenCalledTimes(1);
+        expect(addTracksToPlaylist).toHaveBeenCalledWith(
+            "playlistId",
+            expect.arrayContaining(Array(0).fill("SpotifyURI"))
         );
     });
 });
