2c34daed7c7711bf8400e872968bbc3d3bd59a83
Ellie Wright <ellie952@revature.net>
2025-10-02 15:20:56 -0500

    Add happy path test for getPlaylists in spotifyService

--- src/__test__/spotifyService.test.js
+++ src/__test__/spotifyService.test.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { getUser } = require("../service/spotifyService.js");
+const { getUser, getPlaylists } = require("../service/spotifyService.js");
 const { getAuthHeaders } = require("../util/auth.js");
 
 // Mock getAuthHeaders()
@@ -11,6 +11,10 @@ jest.mock("../util/auth.js", () => ({
 jest.mock("axios");
 
 describe("Spotify service layer", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     describe("getUser function", () => {
         test("Successfully returns user information", async () => {
             // Arrange
@@ -22,6 +26,42 @@ describe("Spotify service layer", () => {
 
             // Assert
             expect(result).toBe(mockUser);
+
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(getAuthHeaders).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe("getPlaylists function", () => {
+        test("Successfully returns all current user's playlists", async () => {
+            // Arrange
+            const mockPlaylists = {
+                items: [
+                    {
+                        id: "playlistId 1",
+                        public: true,
+                        collaborative: false,
+                        name: "Playlist 1"
+                    },
+                    {
+                        id: "playlistId 2",
+                        public: false,
+                        collaborative: true,
+                        name: "Playlist 2"
+                    }
+                ]
+            }
+            axios.get.mockResolvedValueOnce({ data: mockPlaylists });
+
+            // Act
+            const result = await getPlaylists();
+
+            // Assert
+            expect(result).toBe(mockPlaylists);
+            expect(result.items).toHaveLength(2);
+
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(getAuthHeaders).toHaveBeenCalledTimes(1);
         });
     });
 });
