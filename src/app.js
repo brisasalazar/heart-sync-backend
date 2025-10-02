2cc97d1541d8973b1b3045c5e6af67f32ae61134
brisasalazar <brisal.853@gmail.com>
2025-10-01 23:13:30 -0500

    post service test implementation

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 // imports 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const {logger, loggerMiddleware} = require('./util/logger');
 const {authenticateToken} = require("./util/jwt");
 
 // import the necessary functions from the user controller
 const userController = require("./controller/userController");
+const postController = require("./postController");
 
 const PORT = 3000;
 
+function loggerMiddleware(req, res, next){
+    logger.info(`Incoming ${req.method} : ${req.url}`);
+    next();
+}
+
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(loggerMiddleware);
 app.use(express.static("static"));
 
 app.use("/users", userController);
+app.use("/posts", postController);
 
 app.get("/", (req, res) => {
     res.send("Home Page");
