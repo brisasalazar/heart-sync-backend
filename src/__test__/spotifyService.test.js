a11beb73edcf326c35421ef5ef66095fc646565e
brisasalazar <brisal.853@gmail.com>
2025-10-02 16:16:49 -0500

    more spotify service tests.

--- src/__test__/spotifyService.test.js
+++ src/__test__/spotifyService.test.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { getUser } = require("../service/spotifyService.js");
+const { getUser, getTokenInfo, refreshToken, getPlaylists, createPlaylist,  getTrackURI, addTracksToPlaylist} = require("../service/spotifyService.js");
 const { getAuthHeaders } = require("../util/auth.js");
 
 // Mock getAuthHeaders()
@@ -9,6 +9,7 @@ jest.mock("../util/auth.js", () => ({
 
 // Mock axios library
 jest.mock("axios");
+//ellie working on happy path cases
 
 describe("Spotify service layer", () => {
     describe("getUser function", () => {
@@ -23,5 +24,47 @@ describe("Spotify service layer", () => {
             // Assert
             expect(result).toBe(mockUser);
         });
+
+        test("returns nulls when error occurs", async()=>{
+            axios.get.mockRejectedValue(new Error("error"));
+
+            const result = await getUser();
+
+            expect(result).toBeNull();
+            expect(logger.error).toHaveBeenCalledWith(new Error("error"))
+        });
+
+
     });
+
+    describe("getTokenInfo()",()=>{
+        test("should return null with invalid input params", async()=>{
+            axios.get.mockResolvedValue(null);
+
+            const result = await getTokenInfo(null);
+
+            expect(result).toBeNull();
+        });
+    })
+
+    describe("refreshToken()",()=>{
+        test("should return null with invalid input params", async()=>{
+            axios.get.mockResolvedValue(null);
+
+            const result = await refreshToken(null);
+
+            expect(result).toBeNull();
+        });
+    })
+
+    describe("getPlaylists()",()=>{
+        test("should return null with invalid input params", async()=>{
+            axios.get.mockResolvedValue(null);
+
+            const result = await getPlaylists(null);
+
+            expect(axios.get).toThrowError();
+            expect(result).toBeNull();
+        });
+    })
 });
