bb42151dfe32eac74a5a58fed810e200d6e506b6
brisasalazar <brisal.853@gmail.com>
2025-10-17 20:00:08 -0500

    changes to scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/heart-sync-backend
-
-if [ ! -f ".env" ]; then
-  echo "ERROR: .env file not found"
-  exit 1
-else
-  echo ".env file found."
-fi
-
 npm install
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/heart-sync-backend
+
+if [ -f ".env" ]; then
+    set -o allexport
+    source .env
+    set +o allexport
+    echo ".env loaded"
+else
+    echo "ERROR: .env not found!"
+    exit 1
+fi
+
 node src/app.js > /dev/null 2>&1 &
