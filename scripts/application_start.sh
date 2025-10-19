2fd6a1f636d1daa7524501af72d831578aea6ac8
brisasalazar <brisal.853@gmail.com>
2025-10-18 20:56:22 -0500

    application start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/heart-sync-backend
 
-if [ -f ".env" ]; then
-    set -o allexport
-    source .env
-    set +o allexport
-    echo ".env loaded"
-else
-    echo "ERROR: .env not found!"
-    exit 1
-fi
+# if [ -f ".env" ]; then
+#     set -o allexport
+#     source .env
+#     set +o allexport
+#     echo ".env loaded"
+# else
+#     echo "ERROR: .env not found!"
+#     exit 1
+# fi
 
-node app.js > /dev/null 2>&1 &
+node src/app.js > /dev/null 2>&1 &
